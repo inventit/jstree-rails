1128d2273fe0dd51b0a70b4a0de0a3bcea256807
Toshiyuki HINA <toshiyuki.hina@gmail.com>
2018-10-12 15:27:02 +0900

    initial commit

---
+++ lib/jstree/rails.rb
@@ -0,0 +1,7 @@
+require "jstree/rails/version"
+
+module Jstree
+  module Rails
+    # Your code goes here...
+  end
+end
---
+++ lib/jstree/rails/version.rb
@@ -0,0 +1,5 @@
+module Jstree
+  module Rails
+    VERSION = "0.1.0"
+  end
+end
