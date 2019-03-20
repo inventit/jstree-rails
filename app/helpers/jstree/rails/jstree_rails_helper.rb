afc595d34e8203263edbcc720d71caca2dc5dc17
Toshiyuki HINA <thina@yourinventit.com>
2019-03-20 18:54:37 +0900

    Accept block

--- app/helpers/jstree/rails/jstree_rails_helper.rb
+++ app/helpers/jstree/rails/jstree_rails_helper.rb
@@ -6,14 +6,14 @@ module Jstree::Rails::JstreeRailsHelper
   # - (REQUIRED) :id A node ID. This must be unique amoung all of tree nodes.
   # - (REQUIRED) :text A node text which is shown.
   # - (OPTIONAL) :data A hash value to be set to the data attributes of the tree node.
-  def jstree_html_tag(jstree_id, jstree_nodes=[])
+  def jstree_html_tag(jstree_id, jstree_nodes=[], &block)
     root = jstree_nodes.find { |g| g[:parent] == "#" }
 
     content_tag :div, id: jstree_id do
       content_tag :ul do
-        content_tag :li, id: root[:id], data: root[:data] || {} do
+        content_tag :li, content_options(root, &block) do
           concat root[:text]
-          concat jstree_node_tag(root, jstree_nodes)
+          concat jstree_node_tag(root, jstree_nodes, &block)
         end
       end
     end
@@ -21,18 +21,27 @@ module Jstree::Rails::JstreeRailsHelper
 
   private
 
-  def jstree_node_tag(parent, jstree_nodes)
+  def jstree_node_tag(parent, jstree_nodes, &block)
     children = jstree_nodes.select { |g| g[:parent] == parent[:id] }
 
     content_tag :ul do
       children.each do |child|
         concat (
-          content_tag :li, id: child[:id], data: child[:data] || {} do
+          content_tag :li, content_options(child, &block) do
             concat child[:text]
-            concat jstree_node_tag child, jstree_nodes
+            concat jstree_node_tag(child, jstree_nodes, &block)
           end
         )
       end
     end
+  end
+
+  def content_options(node_hash, &block)
+    options = {
+      id: node_hash[:id],
+      data: node_hash[:data] || {}
+    }
+    yield options if block_given?
+    options
   end
 end
